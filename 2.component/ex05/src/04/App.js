3e9843dac570ef20b5c24cf4d330ff2b66f59a3e
yyhjin <wendy0301@naver.com>
2023-11-03 16:56:29 +0900

    timer 04

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -4,22 +4,31 @@ import Clock from "./Clock";
 
 export default function App() {
     const [ticks, setTicks] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
 
     useEffect(() => {
-        setInterval(() => {
+        const timer = setInterval(() => {
             /* 시간 */
 
             setTicks(ticks + 1);
+
+            const date = new Date();
+            setHours(date.getHours());
+            setMinutes(date.getMinutes());
+            setSeconds(date.getSeconds());
         }, 1000);
-    }, []);
+        return () => clearInterval(timer);
+    }, [ticks]);
 
     return (
         <>
             <Clock
                 message={`ex04: ticks ${ticks}`}
-                hours={"15"}
-                minutes={"04"}
-                seconds={"02"}
+                hours={hours}
+                minutes={minutes}
+                seconds={seconds}
             />
         </>
     );
