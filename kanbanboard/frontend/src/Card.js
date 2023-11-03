ac9a1e93e60fd2399fc15da768e84330b75c5841
yyhjin <wendy0301@naver.com>
2023-11-03 14:39:48 +0900

    kanbanboard #3

---
+++ kanbanboard/frontend/src/Card.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import styles from "./assets/scss/Card.scss";
+import TaskList from "./TaskList";
+
+const Card = ({ no, title, description, tasks }) => {
+    const [showDetails, setShowDetails] = useState(false);
+
+    return (
+        <div className={styles.Card}>
+            <div
+                className={
+                    showDetails
+                        ? [styles.Card__Title, styles.Card__Title__open].join(
+                              " "
+                          )
+                        : styles.Card__Title
+                }
+                onClick={(e) => {
+                    setShowDetails(!showDetails);
+                }}
+            >
+                {title}
+            </div>
+            {showDetails ? (
+                <div className={styles.Card__Details}>
+                    {description}
+                    <TaskList tasks={tasks} />
+                </div>
+            ) : null}
+        </div>
+    );
+};
